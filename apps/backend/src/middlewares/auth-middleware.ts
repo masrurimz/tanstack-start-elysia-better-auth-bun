6af21ba5f649988476ac35c3275cc5d72b6c50d7
Muhammad Zahid Masruri <masruri03@gmail.com>
2025-04-06 19:43:57 +0700

    fix(auth): :bug: fix auth middleware response

--- apps/backend/src/middlewares/auth-middleware.ts
+++ apps/backend/src/middlewares/auth-middleware.ts
@@ -1,20 +1,29 @@
+import Elysia from "elysia";
 import { auth } from "~/libs/better-auth/server";
 
 // user middleware (compute user and session and pass to routes)
-const userMiddleware = async (request: Request) => {
-	const session = await auth.api.getSession({ headers: request.headers });
+const betterAuthMiddleware = new Elysia({ name: "better-auth" })
+	.mount(auth.handler)
+	.macro({
+		auth: {
+			async resolve({ error, request: { headers } }) {
+				const session = await auth.api.getSession({
+					headers,
+				});
 
-	if (!session) {
-		return {
-			user: null,
-			session: null,
-		};
-	}
+				if (!session)
+					return error(401, {
+						success: false,
+						message:
+							"Unauthorized: Please check your credentials and permissions",
+					});
 
-	return {
-		user: session.user,
-		session: session.session,
-	};
-};
+				return {
+					user: session.user,
+					session: session.session,
+				};
+			},
+		},
+	});
 
-export { userMiddleware };
+export { betterAuthMiddleware };
