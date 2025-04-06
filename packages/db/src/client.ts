585ee43dfdb494805fb9de18b799280ec06841c2
Muhammad Zahid Masruri <masruri03@gmail.com>
2025-04-06 08:46:16 +0700

    refactor(tooling): :recycle: migrate to eslint and prettier

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,22 +1,22 @@
-import { createClient } from '@libsql/client'
-import { drizzle } from 'drizzle-orm/libsql'
-import { migrate } from 'drizzle-orm/libsql/migrator'
+import { createClient } from '@libsql/client';
+import { drizzle } from 'drizzle-orm/libsql';
+import { migrate } from 'drizzle-orm/libsql/migrator';
 
-import * as schema from './schema'
+import * as schema from './schema';
 
 if (!process.env.DATABASE_URL) {
-	throw new Error('DATABASE_URL is required')
+  throw new Error('DATABASE_URL is required');
 }
 
 export const client = createClient({
-	url: process.env.DATABASE_URL,
-	authToken: process.env.DATABASE_AUTH_TOKEN,
-})
+  url: process.env.DATABASE_URL,
+  authToken: process.env.DATABASE_AUTH_TOKEN,
+});
 
 export const db = drizzle(client, {
-	schema,
-})
+  schema,
+});
 
 await migrate(db, {
-	migrationsFolder: `${process.env.ROOT_DIR}/packages/db/migrations`,
-})
+  migrationsFolder: `${process.env.ROOT_DIR}/packages/db/migrations`,
+});
